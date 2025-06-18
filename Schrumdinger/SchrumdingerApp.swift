1358b975abb79ca8c0682f11cc1bd1798c4d66f4
Tejeshwer Singh <tejeshwersingh2504@gmail.com>
2025-06-18 18:16:27 +0530

    Initial commit

---
+++ Schrumdinger/SchrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchrumdingerApp.swift
+//  Schrumdinger
+//
+//  Created by Tejeshwer Singh on 18/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SchrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
